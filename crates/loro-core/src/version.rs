5444da75af00e37f147cb8ca8d8308d08be729f2
Zixuan Chen <remch183@outlook.com>
2022-08-03 22:21:00 +0800

    feat: cmp vv

--- crates/loro-core/src/version.rs
+++ crates/loro-core/src/version.rs
@@ -1,4 +1,5 @@
 use std::{
+    cmp::Ordering,
     collections::HashMap,
     ops::{Deref, DerefMut},
 };
@@ -21,6 +22,48 @@ impl Deref for VersionVector {
 
     fn deref(&self) -> &Self::Target {
         &self.0
+    }
+}
+
+impl PartialOrd for VersionVector {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        let mut self_greater = true;
+        let mut other_greater = true;
+        let mut eq = true;
+        for (client_id, other_end) in other.iter() {
+            if let Some(self_end) = self.get(client_id) {
+                if self_end < other_end {
+                    self_greater = false;
+                    eq = false;
+                }
+                if self_end > other_end {
+                    other_greater = false;
+                    eq = false;
+                }
+            } else {
+                self_greater = false;
+                eq = false;
+            }
+        }
+
+        for (client_id, _) in self.iter() {
+            if other.contains_key(client_id) {
+                continue;
+            } else {
+                other_greater = false;
+                eq = false;
+            }
+        }
+
+        if eq {
+            Some(Ordering::Equal)
+        } else if self_greater {
+            Some(Ordering::Greater)
+        } else if other_greater {
+            Some(Ordering::Less)
+        } else {
+            None
+        }
     }
 }
 
@@ -101,4 +144,30 @@ impl From<Vec<ID>> for VersionVector {
 pub(crate) struct TotalOrderStamp {
     pub(crate) lamport: Lamport,
     pub(crate) client_id: ClientID,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    mod cmp {
+        use super::*;
+        #[test]
+        fn test() {
+            let a: VersionVector = vec![ID::new(1, 1), ID::new(2, 2)].into();
+            let b: VersionVector = vec![ID::new(1, 1), ID::new(2, 2)].into();
+            assert_eq!(a.partial_cmp(&b), Some(Ordering::Equal));
+
+            let a: VersionVector = vec![ID::new(1, 2), ID::new(2, 1)].into();
+            let b: VersionVector = vec![ID::new(1, 1), ID::new(2, 2)].into();
+            assert_eq!(a.partial_cmp(&b), None);
+
+            let a: VersionVector = vec![ID::new(1, 2), ID::new(2, 3)].into();
+            let b: VersionVector = vec![ID::new(1, 1), ID::new(2, 2)].into();
+            assert_eq!(a.partial_cmp(&b), Some(Ordering::Greater));
+
+            let a: VersionVector = vec![ID::new(1, 0), ID::new(2, 2)].into();
+            let b: VersionVector = vec![ID::new(1, 1), ID::new(2, 2)].into();
+            assert_eq!(a.partial_cmp(&b), Some(Ordering::Less));
+        }
+    }
 }
