fea828811bb68bc686b48a83c33160480e0d737d
Zixuan Chen <remch183@outlook.com>
2022-10-13 14:49:58 +0800

    chore: bench utils

--- crates/loro-core/examples/test.rs
+++ crates/loro-core/examples/test.rs
@@ -7,7 +7,7 @@ fn main() {
     use crdt_list::test::Action::*;
     use loro_core::container::text::tracker::yata::YataImpl;
     let mut actions = vec![];
-    for i in 0..10000_usize {
+    for i in 0..500_usize {
         actions.push(if i % 2 == 0 {
             NewOp {
                 client_id: i as u8,
@@ -22,6 +22,9 @@ fn main() {
         })
     }
 
-    test::test_with_actions::<YataImpl>(5, 100, actions);
+    for _ in 0..1000 {
+        test::test_with_actions::<YataImpl>(5, 100, actions.clone());
+    }
+
     println!("HAHA");
 }
