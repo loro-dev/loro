0fff16faf3e3699315fd421f34149b9616b9a83e
Zixuan Chen <remch183@outlook.com>
2022-11-15 17:41:55 +0800

    test: add gzip size test

--- crates/loro-core/examples/encoding.rs
+++ crates/loro-core/examples/encoding.rs
@@ -1,6 +1,10 @@
-use std::{io::Read, time::Instant};
+use std::{
+    io::{Read, Write},
+    time::Instant,
+};
 
-use flate2::read::GzDecoder;
+use flate2::write::GzEncoder;
+use flate2::{read::GzDecoder, Compression};
 use loro_core::{configure::Configure, container::registry::ContainerWrapper, LoroCore};
 use serde_json::Value;
 const RAW_DATA: &[u8; 901823] = include_bytes!("../benches/automerge-paper.json.gz");
@@ -47,7 +51,7 @@ fn main() {
     let mut last = 100;
     let mut count = 0;
     let mut max_count = 0;
-    for byte in buf {
+    for &byte in buf.iter() {
         if byte == last {
             count += 1;
             if count > max_count {
@@ -60,4 +64,8 @@ fn main() {
     }
 
     println!("Longest continuous bytes length {}", max_count);
+    let mut e = GzEncoder::new(Vec::new(), Compression::new(6));
+    e.write_all(&buf).unwrap();
+    let result = e.finish().unwrap();
+    println!("GZipped Size {}", result.len());
 }
