8e8af7d4c6842e44bb70f2555dd48b9b1d5a1782
Zixuan Chen <remch183@outlook.com>
2022-11-18 00:07:06 +0800

    chore: remove log

--- crates/loro-core/src/container/text/text_container.rs
+++ crates/loro-core/src/container/text/text_container.rs
@@ -280,8 +280,6 @@ impl Container for TextContainer {
             debug_log!("NEW Tracker");
             self.tracker = Tracker::new(vv.clone(), Counter::MAX / 2);
         }
-
-        dbg!(&self.tracker);
     }
 
     fn track_apply(&mut self, rich_op: &RichOp) {
