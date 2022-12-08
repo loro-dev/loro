3eff9a2091ebbdaa07a8bc9cec7a028df96fb71f
Zixuan Chen <remch183@outlook.com>
2022-12-08 23:02:44 +0800

    bench: add benchmark for many concurrent actors

---
+++ crates/loro-core/examples/many_actors.rs
@@ -0,0 +1,20 @@
+use loro_core::{LoroCore, LoroValue};
+
+fn main() {
+    let mut actors: Vec<_> = (0..200).map(|_| LoroCore::default()).collect();
+    for (i, actor) in actors.iter_mut().enumerate() {
+        let mut list = actor.get_list("list");
+        let value: LoroValue = i.to_string().into();
+        list.insert(actor, 0, value).unwrap();
+    }
+
+    for i in 1..actors.len() {
+        let (a, b) = arref::array_mut_ref!(&mut actors, [0, i]);
+        a.import(b.export(a.vv()));
+    }
+
+    for i in 1..actors.len() {
+        let (a, b) = arref::array_mut_ref!(&mut actors, [0, i]);
+        b.import(a.export(b.vv()));
+    }
+}
