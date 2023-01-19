43cb9cc6db841be4f9e1bedcd0411698af15fa77
leeeon233 <leeeon233@gmail.com>
2023-01-19 12:19:08 +0800

    feat: add prelim struct

--- crates/loro-internal/src/prelim.rs
+++ crates/loro-internal/src/prelim.rs
@@ -1,6 +1,7 @@
 use std::sync::{Mutex, Weak};
 
 use enum_as_inner::EnumAsInner;
+use fxhash::FxHashMap;
 
 use crate::{
     container::registry::ContainerInstance, context::Context, ContainerType, LoroError, LoroValue,
@@ -85,5 +86,67 @@ impl From<i32> for PrelimValue {
 impl From<f64> for PrelimValue {
     fn from(v: f64) -> Self {
         PrelimValue::Value(v.into())
+    }
+}
+
+pub struct PrelimText(pub String);
+
+impl Prelim for PrelimText {
+    fn convert_value(self) -> Result<(PrelimValue, Option<Self>), LoroError> {
+        Ok((PrelimValue::Container(ContainerType::Text), Some(self)))
+    }
+
+    fn integrate<C: Context>(
+        self,
+        ctx: &C,
+        container: Weak<Mutex<ContainerInstance>>,
+    ) -> Result<(), LoroError> {
+        let text = container.upgrade().unwrap();
+        let mut text = text.try_lock().unwrap();
+        let text = text.as_text_mut().unwrap();
+        text.insert(ctx, 0, &self.0);
+        Ok(())
+    }
+}
+
+pub struct PrelimList(pub Vec<LoroValue>);
+
+impl Prelim for PrelimList {
+    fn convert_value(self) -> Result<(PrelimValue, Option<Self>), LoroError> {
+        Ok((PrelimValue::Container(ContainerType::List), Some(self)))
+    }
+
+    fn integrate<C: Context>(
+        self,
+        ctx: &C,
+        container: Weak<Mutex<ContainerInstance>>,
+    ) -> Result<(), LoroError> {
+        let list = container.upgrade().unwrap();
+        let mut list = list.try_lock().unwrap();
+        let list = list.as_list_mut().unwrap();
+        list.insert_batch(ctx, 0, self.0);
+        Ok(())
+    }
+}
+
+pub struct PrelimMap(pub FxHashMap<String, LoroValue>);
+
+impl Prelim for PrelimMap {
+    fn convert_value(self) -> Result<(PrelimValue, Option<Self>), LoroError> {
+        Ok((PrelimValue::Container(ContainerType::Map), Some(self)))
+    }
+
+    fn integrate<C: Context>(
+        self,
+        ctx: &C,
+        container: Weak<Mutex<ContainerInstance>>,
+    ) -> Result<(), LoroError> {
+        let map = container.upgrade().unwrap();
+        let mut map = map.try_lock().unwrap();
+        let map = map.as_map_mut().unwrap();
+        for (key, value) in self.0.into_iter() {
+            map.insert(ctx, key.into(), value)?;
+        }
+        Ok(())
     }
 }
