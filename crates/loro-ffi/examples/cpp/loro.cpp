ac961066da50cdfbd96ecf14f1a4ddb520098b49
leeeon233 <leeeon233@gmail.com>
2023-01-28 19:59:37 +0800

    feat: impl C ffi

---
+++ crates/loro-ffi/examples/cpp/loro.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+extern "C" {
+    #include "../../target/loro_ffi.h"
+};
+
+int main(void) {
+  LoroCore* loro = loro_new();
+  Text* text = loro_get_text(loro, "text");
+  text_insert(text, loro, 0, "abc");
+  char* str = text_value(text);
+  printf("%s", str);
+  text_free(text);
+  loro_free(loro);
+}
--- crates/loro-ffi/src/lib.rs
+++ crates/loro-ffi/src/lib.rs
@@ -1,4 +1,9 @@
-use loro_core::LoroCore;
+use std::ffi::{c_char, c_uint, CStr, CString};
+
+pub type LoroCore = loro_core::LoroCore;
+pub type Text = loro_core::Text;
+pub type List = loro_core::List;
+pub type Map = loro_core::Map;
 
 /// create Loro with a random unique client id
 #[no_mangle]
@@ -12,4 +17,43 @@ pub unsafe extern "C" fn loro_free(loro: *mut LoroCore) {
     if !loro.is_null() {
         drop(Box::from_raw(loro));
     }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn loro_get_text(loro: *mut LoroCore, id: *const c_char) -> *mut Text {
+    assert!(!loro.is_null());
+    assert!(!id.is_null());
+    let id = CStr::from_ptr(id).to_str().unwrap();
+    let text = loro.as_mut().unwrap().get_text(id);
+    Box::into_raw(Box::new(text))
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn text_free(text: *mut Text) {
+    if !text.is_null() {
+        drop(Box::from_raw(text));
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn text_insert(
+    text: *mut Text,
+    ctx: *const LoroCore,
+    pos: *const c_uint,
+    value: *const c_char,
+) {
+    assert!(!text.is_null());
+    assert!(!ctx.is_null());
+    let text = text.as_mut().unwrap();
+    let ctx = ctx.as_ref().unwrap();
+    let value = CStr::from_ptr(value).to_str().unwrap();
+    text.insert(ctx, pos as usize, value).unwrap();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn text_value(text: *mut Text) -> *mut c_char {
+    assert!(!text.is_null());
+    let text = text.as_mut().unwrap();
+    let value = text.get_value().as_string().unwrap().to_string();
+    CString::new(value).unwrap().into_raw()
 }
