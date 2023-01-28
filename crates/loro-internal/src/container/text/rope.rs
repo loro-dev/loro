f24d5193dc31d81f22776485d2e53dfea0d48bd4
leeeon233 <leeeon233@gmail.com>
2023-01-28 17:13:57 +0800

    feat: text utf16

--- crates/loro-internal/src/container/text/rope.rs
+++ crates/loro-internal/src/container/text/rope.rs
@@ -74,6 +74,9 @@ impl Rope {
             loop {
                 match node {
                     Node::Internal(internal_node) => {
+                        if index == 0{
+                            return 0
+                        }
                         let result = find_pos_internal(internal_node, index, &src_cache);
                         if !result.found {
                             unreachable!();
