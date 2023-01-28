95309db710873156f3650591dd033571ee07ffca
leeeon233 <leeeon233@gmail.com>
2023-01-28 19:59:37 +0800

    feat: impl C ffi

--- crates/loro-ffi/build.rs
+++ crates/loro-ffi/build.rs
@@ -1,35 +1,8 @@
-extern crate cbindgen;
-
-use cbindgen::Config;
-use std::env;
-use std::path::PathBuf;
-
 fn main() {
-    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-
-    let package_name = env::var("CARGO_PKG_NAME").unwrap();
-    let output_file = target_dir()
-        .join(format!("{}.hpp", package_name))
-        .display()
-        .to_string();
-
-    let config = Config {
-        namespace: Some(String::from("loro_ffi")),
-        ..Default::default()
-    };
-
+    let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let config = cbindgen::Config::from_file("cbindgen.toml")
+        .expect("Unable to find cbindgen.toml configuration file");
     cbindgen::generate_with_config(&crate_dir, config)
         .unwrap()
-        .write_to_file(&output_file);
-}
-
-/// Find the location of the `target/` directory. Note that this may be
-/// overridden by `cmake`, so we also need to check the `CARGO_TARGET_DIR`
-/// variable.
-fn target_dir() -> PathBuf {
-    if let Ok(target) = env::var("CARGO_TARGET_DIR") {
-        PathBuf::from(target)
-    } else {
-        PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("target")
-    }
+        .write_to_file("target/loro_ffi.h");
 }
