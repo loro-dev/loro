ea921e4c8fb40aeebc2bcf5c0d1eeaadd208ccae
leeeon233 <leeeon233@gmail.com>
2023-02-17 09:51:29 +0800

    fix: remove counter & lamport from RleUpdates encoding

--- crates/loro-internal/src/log_store/encoding/encode_changes.rs
+++ crates/loro-internal/src/log_store/encoding/encode_changes.rs
@@ -1,4 +1,5 @@
 use fxhash::FxHashMap;
+use itertools::Itertools;
 use rle::{HasLength, RleVec};
 use serde::{Deserialize, Serialize};
 use serde_columnar::{columnar, from_bytes, to_vec};
@@ -76,7 +77,7 @@ impl DepsEncoding {
 
 #[columnar(ser, de)]
 #[derive(Debug, Serialize, Deserialize)]
-struct Encoded {
+struct DocEncoding {
     #[columnar(type = "vec")]
     changes: Vec<ChangeEncoding>,
     #[columnar(type = "vec")]
@@ -86,6 +87,8 @@ struct Encoded {
     clients: Clients,
     containers: Containers,
     keys: Vec<InternalString>,
+    start_counter: Vec<Counter>,
+    start_lamport: Vec<Lamport>,
 }
 
 #[instrument(skip_all)]
@@ -100,21 +103,26 @@ pub(super) fn encode_changes(store: &LogStore, vv: &VersionVector) -> Result<Vec
     let mut diff_changes = Vec::new();
     let self_vv = store.vv();
     let diff = self_vv.diff(vv);
+
+    let mut start_counter = Vec::new();
+    let mut start_lamport = Vec::new();
+
     for span in diff.left.iter() {
         let changes = store.get_changes_slice(span.id_span());
-        for change in changes.into_iter() {
-            diff_changes.push(change);
-        }
-    }
-
-    for change in &diff_changes {
-        let client_id = change.id.client_id;
+        change_num += changes.len();
+        let client_id = *span.0;
         client_id_to_idx.entry(client_id).or_insert_with(|| {
             let idx = clients.len() as ClientIdx;
             clients.push(client_id);
             idx
         });
-        change_num += 1;
+        start_counter.push(changes.first().unwrap().id.counter);
+        start_lamport.push(changes.first().unwrap().lamport);
+
+        diff_changes.extend(changes);
+    }
+
+    for change in &diff_changes {
         for deps in change.deps.iter() {
             client_id_to_idx.entry(deps.client_id).or_insert_with(|| {
                 let idx = clients.len() as ClientIdx;
@@ -197,13 +205,15 @@ pub(super) fn encode_changes(store: &LogStore, vv: &VersionVector) -> Result<Vec
         });
     }
 
-    let encoded = Encoded {
+    let encoded = DocEncoding {
         changes,
         ops,
         deps,
         clients,
         containers: container_ids,
         keys,
+        start_counter,
+        start_lamport,
     };
 
     to_vec(&encoded).map_err(|e| LoroError::DecodeError(e.to_string().into()))
@@ -223,101 +233,112 @@ pub(super) fn decode_changes(
 pub(super) fn decode_changes_to_inner_format(
     input: &[u8],
 ) -> Result<RemoteClientChanges, LoroError> {
-    let encoded: Encoded =
+    let encoded: DocEncoding =
         from_bytes(input).map_err(|e| LoroError::DecodeError(e.to_string().into()))?;
 
-    let Encoded {
+    let DocEncoding {
         changes: change_encodings,
         ops,
         deps,
         clients,
         containers,
         keys,
+        start_counter,
+        start_lamport,
     } = encoded;
 
     let mut op_iter = ops.into_iter();
     let mut changes = FxHashMap::default();
     let mut deps_iter = deps.into_iter();
 
-    for change_encoding in change_encodings {
-        let ChangeEncoding {
-            client_idx,
-            counter,
-            lamport,
-            timestamp,
-            op_len,
-            deps_len,
-        } = change_encoding;
+    for (client_idx, this_change_encodings) in
+        &change_encodings.into_iter().group_by(|c| c.client_idx)
+    {
+        let mut counter = start_counter[client_idx as usize];
+        let mut lamport = start_lamport[client_idx as usize];
+        for change_encoding in this_change_encodings {
+            let ChangeEncoding {
+                client_idx,
+                counter: this_counter,
+                lamport: this_lamport,
+                timestamp,
+                op_len,
+                deps_len,
+            } = change_encoding;
 
-        let client_id = clients[client_idx as usize];
-        let mut ops = RleVec::<[RemoteOp; 2]>::new();
-        let deps = (0..deps_len)
-            .map(|_| {
-                let raw = deps_iter.next().unwrap();
-                ID::new(clients[raw.client_idx as usize], raw.counter)
-            })
-            .collect();
+            let client_id = clients[client_idx as usize];
+            let mut ops = RleVec::<[RemoteOp; 2]>::new();
+            let deps = (0..deps_len)
+                .map(|_| {
+                    let raw = deps_iter.next().unwrap();
+                    ID::new(clients[raw.client_idx as usize], raw.counter)
+                })
+                .collect();
 
-        let mut op_counter = counter;
-        for op in op_iter.by_ref().take(op_len as usize) {
-            let OpEncoding {
-                container: container_idx,
-                prop,
-                value,
-                gc,
-            } = op;
-            let container_id = containers[container_idx].clone();
+            let mut delta = 0;
+            for op in op_iter.by_ref().take(op_len as usize) {
+                let OpEncoding {
+                    container: container_idx,
+                    prop,
+                    value,
+                    gc,
+                } = op;
+                let container_id = containers[container_idx].clone();
 
-            let container_type = container_id.container_type();
-            let content = match container_type {
-                ContainerType::Map => {
-                    let key = keys[prop].clone();
-                    RemoteContent::Map(MapSet { key, value })
-                }
-                ContainerType::List | ContainerType::Text => {
-                    let pos = prop;
-                    let list_op = match value {
-                        LoroValue::I32(len) => ListOp::Delete(DeleteSpan {
-                            pos: pos as isize,
-                            len: len as isize,
-                        }),
-                        LoroValue::Null => ListOp::Insert {
-                            pos,
-                            slice: ListSlice::Unknown(gc),
-                        },
-                        _ => {
-                            let slice = match value {
-                                LoroValue::String(s) => ListSlice::RawStr(SmString::from(&*s)),
-                                LoroValue::List(v) => ListSlice::RawData(*v),
-                                _ => unreachable!(),
-                            };
-                            ListOp::Insert { slice, pos }
-                        }
-                    };
-                    RemoteContent::List(list_op)
-                }
+                let container_type = container_id.container_type();
+                let content = match container_type {
+                    ContainerType::Map => {
+                        let key = keys[prop].clone();
+                        RemoteContent::Map(MapSet { key, value })
+                    }
+                    ContainerType::List | ContainerType::Text => {
+                        let pos = prop;
+                        let list_op = match value {
+                            LoroValue::I32(len) => ListOp::Delete(DeleteSpan {
+                                pos: pos as isize,
+                                len: len as isize,
+                            }),
+                            LoroValue::Null => ListOp::Insert {
+                                pos,
+                                slice: ListSlice::Unknown(gc),
+                            },
+                            _ => {
+                                let slice = match value {
+                                    LoroValue::String(s) => ListSlice::RawStr(SmString::from(&*s)),
+                                    LoroValue::List(v) => ListSlice::RawData(*v),
+                                    _ => unreachable!(),
+                                };
+                                ListOp::Insert { slice, pos }
+                            }
+                        };
+                        RemoteContent::List(list_op)
+                    }
+                };
+                let remote_op = RemoteOp {
+                    container: container_id,
+                    counter: counter + delta,
+                    contents: vec![content].into(),
+                };
+                delta += remote_op.content_len() as i32;
+                ops.push(remote_op);
+            }
+
+            let change = Change {
+                id: ID { client_id, counter },
+                lamport,
+                timestamp,
+                ops,
+                deps,
             };
-            let remote_op = RemoteOp {
-                container: container_id,
-                counter: op_counter,
-                contents: vec![content].into(),
-            };
-            op_counter += remote_op.content_len() as i32;
-            ops.push(remote_op);
+
+            counter += delta;
+            lamport += delta as u32;
+
+            changes
+                .entry(client_id)
+                .or_insert_with(Vec::new)
+                .push(change);
         }
-
-        let change = Change {
-            id: ID { client_id, counter },
-            lamport,
-            timestamp,
-            ops,
-            deps,
-        };
-
-        changes
-            .entry(client_id)
-            .or_insert_with(Vec::new)
-            .push(change);
     }
     // TODO: using the one with fewer changes to import
     Ok(changes)
