43aa79f1de71a5d850952587eccc1dedb8b53fc8
Zixuan Chen <remch183@outlook.com>
2023-04-02 21:47:37 +0800

    chore: try to fix ci

--- crates/loro-wasm/scripts/build.ts
+++ crates/loro-wasm/scripts/build.ts
@@ -35,7 +35,7 @@ async function build() {
   if (profile !== "dev") {
     await Promise.all(TARGETS.map(async (target) => {
       const cmd =
-        `wasm-opt -O4 ${target}/loro_wasm_bg.wasm -o ${target}/loro_wasm_bg.wasm`;
+        `wasm-opt -O4 ./${target}/loro_wasm_bg.wasm -o ./${target}/loro_wasm_bg.wasm`;
       console.log(">", cmd);
       await Deno.run({ cmd: cmd.split(" "), cwd: LoroWasmDir }).status();
     }));
