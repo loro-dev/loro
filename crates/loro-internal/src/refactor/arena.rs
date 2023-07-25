1ed8ad05be15cf03336a8d9720dae8c1594cbac8
Zixuan Chen <remch183@outlook.com>
2023-07-25 23:06:21 +0800

    perf: reduce shared arena size make it more compact and cache-friendly

--- crates/loro-internal/src/refactor/arena.rs
+++ crates/loro-internal/src/refactor/arena.rs
@@ -20,20 +20,25 @@ use crate::{
     LoroValue,
 };
 
-/// This is shared between [OpLog] and [AppState].
-///
-#[derive(Clone, Default)]
-pub struct SharedArena {
+#[derive(Default)]
+struct InnerSharedArena {
     // The locks should not be exposed outside this file.
     // It might be better to use RwLock in the future
-    container_idx_to_id: Arc<Mutex<Vec<ContainerID>>>,
-    container_id_to_idx: Arc<Mutex<FxHashMap<ContainerID, ContainerIdx>>>,
+    container_idx_to_id: Mutex<Vec<ContainerID>>,
+    container_id_to_idx: Mutex<FxHashMap<ContainerID, ContainerIdx>>,
     /// The parent of each container.
-    parents: Arc<Mutex<FxHashMap<ContainerIdx, Option<ContainerIdx>>>>,
-    text: Arc<Mutex<AppendOnlyBytes>>,
-    text_utf16_len: Arc<AtomicUsize>,
-    values: Arc<Mutex<Vec<LoroValue>>>,
-    root_c_idx: Arc<Mutex<Vec<ContainerIdx>>>,
+    parents: Mutex<FxHashMap<ContainerIdx, Option<ContainerIdx>>>,
+    text: Mutex<AppendOnlyBytes>,
+    text_utf16_len: AtomicUsize,
+    values: Mutex<Vec<LoroValue>>,
+    root_c_idx: Mutex<Vec<ContainerIdx>>,
+}
+
+/// This is shared between [OpLog] and [AppState].
+///
+#[derive(Default, Clone)]
+pub struct SharedArena {
+    inner: Arc<InnerSharedArena>,
 }
 
 pub struct StrAllocResult {
@@ -44,44 +49,50 @@ pub struct StrAllocResult {
 
 impl SharedArena {
     pub fn register_container(&self, id: &ContainerID) -> ContainerIdx {
-        let mut container_id_to_idx = self.container_id_to_idx.lock().unwrap();
+        let mut container_id_to_idx = self.inner.container_id_to_idx.lock().unwrap();
         if let Some(&idx) = container_id_to_idx.get(id) {
             return idx;
         }
 
-        let mut container_idx_to_id = self.container_idx_to_id.lock().unwrap();
+        let mut container_idx_to_id = self.inner.container_idx_to_id.lock().unwrap();
         let idx = container_idx_to_id.len();
         container_idx_to_id.push(id.clone());
         let idx = ContainerIdx::from_index_and_type(idx as u32, id.container_type());
         container_id_to_idx.insert(id.clone(), idx);
         if id.is_root() {
-            self.root_c_idx.lock().unwrap().push(idx);
-            self.parents.lock().unwrap().insert(idx, None);
+            self.inner.root_c_idx.lock().unwrap().push(idx);
+            self.inner.parents.lock().unwrap().insert(idx, None);
         }
         idx
     }
 
     pub fn get_container_id(&self, idx: ContainerIdx) -> Option<ContainerID> {
-        let lock = self.container_idx_to_id.lock().unwrap();
+        let lock = self.inner.container_idx_to_id.lock().unwrap();
         lock.get(idx.to_index() as usize).cloned()
     }
 
     pub fn id_to_idx(&self, id: &ContainerID) -> Option<ContainerIdx> {
-        self.container_id_to_idx.lock().unwrap().get(id).copied()
+        self.inner
+            .container_id_to_idx
+            .lock()
+            .unwrap()
+            .get(id)
+            .copied()
     }
 
     pub fn idx_to_id(&self, id: ContainerIdx) -> Option<ContainerID> {
-        let lock = self.container_idx_to_id.lock().unwrap();
+        let lock = self.inner.container_idx_to_id.lock().unwrap();
         lock.get(id.to_index() as usize).cloned()
     }
 
     /// return utf16 len
     pub fn alloc_str(&self, str: &str) -> StrAllocResult {
-        let mut text_lock = self.text.lock().unwrap();
+        let mut text_lock = self.inner.text.lock().unwrap();
         let start = text_lock.len();
         let utf16_len = count_utf16_chars(str.as_bytes());
         text_lock.push_slice(str.as_bytes());
-        self.text_utf16_len
+        self.inner
+            .text_utf16_len
             .fetch_add(utf16_len, std::sync::atomic::Ordering::SeqCst);
         StrAllocResult {
             start,
@@ -91,26 +102,28 @@ impl SharedArena {
     }
 
     pub fn alloc_str_fast(&self, bytes: &[u8]) {
-        let mut text_lock = self.text.lock().unwrap();
+        let mut text_lock = self.inner.text.lock().unwrap();
         let utf16_len = count_utf16_chars(bytes);
-        self.text_utf16_len
+        self.inner
+            .text_utf16_len
             .fetch_add(utf16_len, std::sync::atomic::Ordering::SeqCst);
         text_lock.push_slice(bytes);
     }
 
     pub fn utf16_len(&self) -> usize {
-        self.text_utf16_len
+        self.inner
+            .text_utf16_len
             .load(std::sync::atomic::Ordering::SeqCst)
     }
 
     pub fn alloc_value(&self, value: LoroValue) -> usize {
-        let mut values_lock = self.values.lock().unwrap();
+        let mut values_lock = self.inner.values.lock().unwrap();
         values_lock.push(value);
         values_lock.len() - 1
     }
 
     pub fn alloc_values(&self, values: impl Iterator<Item = LoroValue>) -> std::ops::Range<usize> {
-        let mut values_lock = self.values.lock().unwrap();
+        let mut values_lock = self.inner.values.lock().unwrap();
         values_lock.reserve(values.size_hint().0);
         let start = values_lock.len();
         for value in values {
@@ -128,12 +141,12 @@ impl SharedArena {
             self.get_container_id(child),
             parent.map(|x| self.get_container_id(x))
         );
-        self.parents.lock().unwrap().insert(child, parent);
+        self.inner.parents.lock().unwrap().insert(child, parent);
     }
 
     pub fn log_hierarchy(&self) {
         if cfg!(debug_assertions) {
-            for (c, p) in self.parents.lock().unwrap().iter() {
+            for (c, p) in self.inner.parents.lock().unwrap().iter() {
                 debug_log::debug_log!(
                     "container {:?} {:?} {:?}",
                     c,
@@ -146,7 +159,13 @@ impl SharedArena {
 
     pub fn get_parent(&self, child: ContainerIdx) -> Option<ContainerIdx> {
         self.log_hierarchy();
-        self.parents.lock().unwrap().get(&child).copied().flatten()
+        self.inner
+            .parents
+            .lock()
+            .unwrap()
+            .get(&child)
+            .copied()
+            .flatten()
     }
 
     /// Call `f` on each ancestor of `container`, including `container` itself.
@@ -159,15 +178,15 @@ impl SharedArena {
     }
 
     pub fn slice_bytes(&self, range: impl RangeBounds<usize>) -> BytesSlice {
-        self.text.lock().unwrap().slice(range)
+        self.inner.text.lock().unwrap().slice(range)
     }
 
     pub fn get_value(&self, idx: usize) -> Option<LoroValue> {
-        self.values.lock().unwrap().get(idx).cloned()
+        self.inner.values.lock().unwrap().get(idx).cloned()
     }
 
     pub fn get_values(&self, range: Range<usize>) -> Vec<LoroValue> {
-        (self.values.lock().unwrap()[range]).to_vec()
+        (self.inner.values.lock().unwrap()[range]).to_vec()
     }
 
     pub fn convert_single_op(
@@ -181,11 +200,11 @@ impl SharedArena {
     }
 
     pub fn is_empty(&self) -> bool {
-        self.container_idx_to_id.lock().unwrap().is_empty()
-            && self.container_id_to_idx.lock().unwrap().is_empty()
-            && self.text.lock().unwrap().is_empty()
-            && self.values.lock().unwrap().is_empty()
-            && self.parents.lock().unwrap().is_empty()
+        self.inner.container_idx_to_id.lock().unwrap().is_empty()
+            && self.inner.container_id_to_idx.lock().unwrap().is_empty()
+            && self.inner.text.lock().unwrap().is_empty()
+            && self.inner.values.lock().unwrap().is_empty()
+            && self.inner.parents.lock().unwrap().is_empty()
     }
 
     fn inner_convert_op(
@@ -264,12 +283,12 @@ impl SharedArena {
     }
 
     pub fn export_containers(&self) -> Vec<ContainerID> {
-        self.container_idx_to_id.lock().unwrap().clone()
+        self.inner.container_idx_to_id.lock().unwrap().clone()
     }
 
     pub fn export_parents(&self) -> Vec<Option<ContainerIdx>> {
-        let parents = self.parents.lock().unwrap();
-        let containers = self.container_idx_to_id.lock().unwrap();
+        let parents = self.inner.parents.lock().unwrap();
+        let containers = self.inner.container_idx_to_id.lock().unwrap();
         containers
             .iter()
             .enumerate()
@@ -282,6 +301,6 @@ impl SharedArena {
     }
 
     pub fn root_containers(&self) -> Vec<ContainerIdx> {
-        self.root_c_idx.lock().unwrap().clone()
+        self.inner.root_c_idx.lock().unwrap().clone()
     }
 }
