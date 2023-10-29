4d87725f32099355d4ece79e7aabde9b8486c0b3
Zixuan Chen <remch183@outlook.com>
2023-10-29 22:13:59 +0800

    chore: rm needless package

--- crates/loro-e2ee/src/lib.rs
+++
@@ -1,5 +0,0 @@
-#![allow(unused)]
-mod encoding;
-mod loro;
-mod raw_change;
-mod raw_store;
--- crates/loro-e2ee/src/loro.rs
+++
@@ -1,9 +0,0 @@
-use loro_internal::{id::PeerID, LoroDoc};
-
-use crate::raw_store::RawStore;
-
-pub struct Loro {
-    pub this_client_id: PeerID,
-    pub raw_store: Option<RawStore>,
-    pub log_store: Option<LoroDoc>,
-}
