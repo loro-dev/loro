116190c81768c10188feafe41d862f98845419cd
Zixuan Chen <remch183@outlook.com>
2023-11-08 14:18:01 +0800

    chore: refine err msg

--- crates/loro-internal/src/container/richtext/richtext_state.rs
+++ crates/loro-internal/src/container/richtext/richtext_state.rs
@@ -1547,7 +1547,7 @@ impl RichtextState {
             "pos: {}, len: {}, self.len(): {}",
             pos,
             len,
-            &self.to_string()
+            &self.len_entity(),
         );
         // PERF: may use cache to speed up
         self.cursor_cache.invalidate();
