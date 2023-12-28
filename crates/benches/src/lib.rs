727b5c2518501d1b15b7a8bec5d5b18704138063
Zixuan Chen <remch183@outlook.com>
2023-12-28 18:00:34 +0800

    Add benchmark utils that simulate drawing workflow (#229)
    * chore: add benchmark utils that simulate drawing workflow
    
    * chore: use markdown as table default style
    
    * chore: init sheet simulating

---
+++ crates/benches/src/lib.rs
@@ -0,0 +1,2 @@
+pub mod draw;
+pub mod sheet;
--- crates/loro/src/lib.rs
+++ crates/loro/src/lib.rs
@@ -186,6 +186,16 @@ impl LoroDoc {
         self.doc.state_vv()
     }
 
+    /// Get the total number of operations in the `OpLog`
+    pub fn len_ops(&self) -> usize {
+        self.doc.len_ops()
+    }
+
+    /// Get the total number of changes in the `OpLog`
+    pub fn len_changes(&self) -> usize {
+        self.doc.len_changes()
+    }
+
     pub fn get_deep_value(&self) -> LoroValue {
         self.doc.get_deep_value()
     }
@@ -320,8 +330,14 @@ impl LoroList {
     }
 
     #[inline]
-    pub fn push(&self, v: LoroValue) -> LoroResult<()> {
-        self.handler.push(v)
+    pub fn push(&self, v: impl Into<LoroValue>) -> LoroResult<()> {
+        self.handler.push(v.into())
+    }
+
+    #[inline]
+    pub fn push_container(&self, c_type: ContainerType) -> LoroResult<Container> {
+        let pos = self.handler.len();
+        Ok(Container::from(self.handler.insert_container(pos, c_type)?))
     }
 
     pub fn for_each<I>(&self, f: I)
