c3d5e9240786ed9d1e8770285cc966c208bf3a4f
Zixuan Chen <remch183@outlook.com>
2024-03-31 02:06:57 +0800

    refactor: rm isAttached type on containers (#303)

--- loro-js/tests/type.test.ts
+++ loro-js/tests/type.test.ts
@@ -5,25 +5,3 @@ test("You shuold not insert a container by using `insert` function", () => {
   const list = new LoroList();
   expectTypeOf(list).not.toMatchTypeOf<Value>();
 });
-
-test("Container attached state", () => {
-  const list = new LoroList();
-  expect(list.isAttached()).toBe(false);
-  expectTypeOf(list.isAttached()).toMatchTypeOf<false>();
-  const doc = new Loro();
-  {
-    const map = doc.getMap("map");
-    expectTypeOf(map.isAttached()).toMatchTypeOf<true>();
-    expectTypeOf(map).toMatchTypeOf<LoroMap<any, true>>();
-  }
-  {
-    const map = new LoroMap();
-    expectTypeOf(map.isAttached()).toMatchTypeOf<false>();
-    expectTypeOf(map).toMatchTypeOf<LoroMap<any, false>>();
-  }
-  {
-    const map = list.insertContainer(0, new LoroMap());
-    expectTypeOf(map.isAttached()).toMatchTypeOf<false>();
-    expectTypeOf(map).toMatchTypeOf<LoroMap<any, false>>();
-  }
-});
