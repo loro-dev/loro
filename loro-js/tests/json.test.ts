4c3f2a9f45412b16268175bea3236f5c5e42c35c
Leon Zhao <leeeon233@gmail.com>
2024-05-27 11:21:32 +0800

    test: migrate from v0.15

--- loro-js/tests/json.test.ts
+++ loro-js/tests/json.test.ts
@@ -20,3 +20,117 @@ it("json encoding", () => {
     console.log(json);
     
 })
+
+it("json decoding", () => {
+    const v15Json = `{
+  "schema_version": 1,
+  "start_vv": {},
+  "end_vv": {
+    "14944917281143706156": 12
+  },
+  "peers": [
+    14944917281143706156
+  ],
+  "changes": [
+    {
+      "id": "0@0",
+      "timestamp": 0,
+      "deps": [],
+      "lamport": 0,
+      "msg": null,
+      "ops": [
+        {
+          "container": "cid:root-text:Text",
+          "content": {
+            "type": "insert",
+            "pos": 0,
+            "text": "123"
+          },
+          "counter": 0
+        },
+        {
+          "container": "cid:root-map:Map",
+          "content": {
+            "type": "insert",
+            "key": "subMap",
+            "value": "🦜:cid:3@0:Map"
+          },
+          "counter": 3
+        },
+        {
+          "container": "cid:3@0:Map",
+          "content": {
+            "type": "insert",
+            "key": "foo",
+            "value": "bar"
+          },
+          "counter": 4
+        },
+        {
+          "container": "cid:root-list:List",
+          "content": {
+            "type": "insert",
+            "pos": 0,
+            "value": [
+              "foo",
+              "🦜"
+            ]
+          },
+          "counter": 5
+        },
+        {
+          "container": "cid:root-tree:Tree",
+          "content": {
+            "type": "move",
+            "target": "7@0",
+            "parent": null
+          },
+          "counter": 7
+        },
+        {
+          "container": "cid:root-tree:Tree",
+          "content": {
+            "type": "move",
+            "target": "8@0",
+            "parent": "7@0"
+          },
+          "counter": 8
+        },
+        {
+          "container": "cid:8@0:Map",
+          "content": {
+            "type": "insert",
+            "key": "tree",
+            "value": "abc"
+          },
+          "counter": 9
+        },
+        {
+          "container": "cid:root-text:Text",
+          "content": {
+            "type": "mark",
+            "start": 0,
+            "end": 3,
+            "style": [
+              "bold",
+              true
+            ],
+            "info": 132
+          },
+          "counter": 10
+        },
+        {
+          "container": "cid:root-text:Text",
+          "content": {
+            "type": "mark_end"
+          },
+          "counter": 11
+        }
+      ]
+    }
+  ]
+}`;
+const doc = new Loro();
+doc.importJSON(v15Json);
+console.log(doc.exportJSON());
+})
