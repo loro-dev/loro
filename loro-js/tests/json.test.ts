5eba0f713460e98e6ec005f73851eccbfa510684
Leon Zhao <leeeon233@gmail.com>
2024-05-27 11:17:39 +0800

    fix: tree id json

---
+++ loro-js/tests/json.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, expectTypeOf, it } from "vitest";
+import { Loro, LoroMap, LoroTree, LoroTreeNode } from "../src";
+
+it("json encoding", () => {
+    const doc = new Loro();
+    const text = doc.getText("text");
+    text.insert(0, "123")
+    const map = doc.getMap("map");
+    const list = doc.getList("list");
+    const tree = doc.getTree("tree");
+    const subMap = map.setContainer("subMap", new LoroMap());
+    subMap.set("foo", "bar");
+    list.push("foo");
+    list.push("🦜");
+    const root = tree.createNode(undefined);
+    const child = tree.createNode(root.id);
+    child.data.set("tree", "abc");
+    text.mark({start:0, end:3}, "bold", true);
+    const json = doc.exportJSON();
+    console.log(json);
+    
+})
