c4663d5a2fae632295675547d4705fab6ab46cc6
Patrick Arminio <patrick.arminio@gmail.com>
2024-07-21 13:07:02 +0200

    Improve error

--- crates/loro-common/src/error.rs
+++ crates/loro-common/src/error.rs
@@ -46,7 +46,7 @@ pub enum LoroError {
     ArgErr(Box<str>),
     #[error("Auto commit has not started. The doc is readonly when detached. You should ensure autocommit is on and the doc and the state is attached.")]
     AutoCommitNotStarted,
-    #[error("You need to specify the style flag for \"({0:?})\" before mark with this key")]
+    #[error("Style configuration missing for \"({0:?})\". Please provide the style configuration using `configTextStyle` on your Loro doc.")]
     StyleConfigMissing(InternalString),
     #[error("Unknown Error ({0})")]
     Unknown(Box<str>),
