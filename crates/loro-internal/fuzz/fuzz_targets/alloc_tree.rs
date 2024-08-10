141467eb4f2c762ac45fe68e3de73119a01c48ec
Zixuan Chen <remch183@outlook.com>
2024-08-11 00:32:06 +0800

    test: chang fuzz rule

--- crates/loro-internal/fuzz/fuzz_targets/alloc_tree.rs
+++ crates/loro-internal/fuzz/fuzz_targets/alloc_tree.rs
@@ -4,5 +4,7 @@ use libfuzzer_sys::fuzz_target;
 use loro_internal::dag::{fuzz_alloc_tree, Interaction};
 
 fuzz_target!(|data: Vec<Interaction>| {
-    fuzz_alloc_tree(10, data);
+    fuzz_alloc_tree(10, data.clone());
+    fuzz_alloc_tree(5, data.clone());
+    fuzz_alloc_tree(2, data);
 });
