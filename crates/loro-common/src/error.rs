a1aee401ea9014f449cc783b61e38bdde01ac2ab
Leon Zhao <leeeon233@gmail.com>
2024-09-09 16:11:54 +0800

    fix: error string

--- crates/loro-common/src/error.rs
+++ crates/loro-common/src/error.rs
@@ -88,7 +88,7 @@ pub enum LoroTreeError {
     TreeNodeNotExist(TreeID),
     #[error("The index({index}) should be <= the length of children ({len})")]
     IndexOutOfBound { len: usize, index: usize },
-    #[error("Fractional index is not enabled, you should enable it on the doc first.")]
+    #[error("Fractional index is not enabled, you should enable it first by `LoroTree::set_enable_fractional_index`")]
     FractionalIndexNotEnabled,
 }
 
