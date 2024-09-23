0635f0a0369a87614661b55c8b66651fbe1c226d
Zixuan Chen <remch183@outlook.com>
2024-09-23 22:22:22 +0800

    test: test failed if use non-cached diff calc

--- crates/loro-internal/src/loro.rs
+++ crates/loro-internal/src/loro.rs
@@ -1166,7 +1166,7 @@ impl LoroDoc {
         }
 
         let mut state = self.state.lock().unwrap();
-        let mut calc = self.diff_calculator.lock().unwrap();
+        let mut calc = DiffCalculator::new(false);
         for &i in frontiers.iter() {
             if !oplog.dag.contains(i) {
                 drop(oplog);
