a7b87471df029f763675616a8554ba661dd28c52
Zixuan Chen <remch183@outlook.com>
2026-07-20 01:52:19 +0800

    chore: remove generated loro-js JavaScript

--- loro-js/src/codec/types.js
+++
@@ -1,8 +0,0 @@
-export const ContainerType = {
-    Map: "map",
-    List: "list",
-    Text: "text",
-    Tree: "tree",
-    MovableList: "movable-list",
-    Counter: "counter",
-};
--- loro-js/src/runtime/index.js
+++
@@ -1,8 +0,0 @@
-export * from "./containers";
-export * from "./document";
-export * from "./ephemeral";
-export { idStrToId, isContainerId, newContainerID, newRootContainerID } from "./ids";
-export * from "./json-updates";
-export * from "./types";
-export * from "./undo";
-export * from "./version-vector";
